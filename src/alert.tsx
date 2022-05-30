00798ab98a3de14961e65cc7c451cce1ab596c0b
JeremyBaucherel <61932395+JeremyBaucherel@users.noreply.github.com>
2022-05-30 10:17:30 +0200

    Update alert.tsx

--- src/alert.tsx
+++ src/alert.tsx
@@ -6,6 +6,7 @@ export interface IAlertBoxProps {
 	icon?: EIcon;
 	title?: string;
 	text?: string;
+	tabText?: string[];
 }
 
 export class AlertBox extends React.PureComponent<IAlertBoxProps, {}> {
@@ -26,13 +27,19 @@ export class AlertBox extends React.PureComponent<IAlertBoxProps, {}> {
 		if (this.props.text) {
 			text = (<p>{this.props.text}</p>);
 		}
-
+		let tabText=[];
+		if (this.props.tabText) {
+			for(let tt in this.props.tabText){
+				tabText.push(<p>{this.props.tabText[tt]}</p>);
+			}
+		}
 		return (
 			<div className="stk-alertbox">
 				<div className="stk-alertbox-icon"><Icon icon={icon} /></div>
 				<div className="stk-alertbox-body">
 					{title}
 					{text}
+					{tabText}
 					{this.props.children}
 				</div>
 			</div>
